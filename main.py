f0bd92801a1ee5ba838c13c9f278da213944dd99
shubhbhatia <shubhambhatiak@gmail.com>
2024-06-25 23:37:28 +0530

    Commit

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ import Orders
 APP_ID = accessTOTP.APP_ID
 access_token = accessTOTP.main()
 
+
+symbol = input("Symbol: ")
+qty = input("Qty: ")
+limitPrice = input("Limit Price: ")
+stopLoss = input("Stop Loss: ")
+
 # get all open positions
 # getPos.getOpenPositions(APP_ID, access_token)
 
 # Orders.getOrderbook(APP_ID, access_token)
-Orders.openNewOrder("ioc", 1, 174, 190, APP_ID, access_token)
+Orders.openNewOrder(symbol, qty, limitPrice, stopLoss, APP_ID, access_token)
