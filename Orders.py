84151b7968019dcc5e6897564e93ef247ae96ad1
shubhbhatia <shubhambhatiak@gmail.com>
2024-06-25 07:46:59 +0530

    Commit

--- Orders.py
+++ Orders.py
@@ -6,8 +6,18 @@ APP_ID = accessTOTP.APP_ID
 access_token = accessTOTP.main()
 fyers = fyersModel.FyersModel(client_id=APP_ID, token=access_token,is_async=False, log_path="")
 
+def getTradeToOpen():
+    file = open("C:\Users\shubhbhatia\Desktop\Trade.txt").readlines()
+    developer_id = ""
+
+    for lines in file:
+        if 'Developer ID' in lines:
+            developer_id = lines.split(":")[-1].strip()
+
+    print
+    developer_id
 def openNewOrder():
-    symbol = input("Symbol: ")
+    symbol = input("Symbol: " or "jublfood")
     symbol = "NSE:"+symbol.upper()+"-EQ"
     print(symbol)
 
@@ -20,28 +30,33 @@ def openNewOrder():
     # CO
     # BO
 
-    type = input("Type (1-LO 2-MO 3-SL/M 4-SL/L): ")
+    # type = input("Type (1-LO 2-MO 3-SL/M 4-SL/L): ")
     qty = int(input("Qty: ") or 1)
+    # price = input("Price: ")
     limitPrice = input("Limit Price: ")
-    stopPrice = input("Stop Price: ")
-    takeProfit = input("Take Profit: ")
-    side = input("Side (1-Buy 2-Sell): ")
-    productType = input("Product Type (Intraday/CO/BO): ")
+    # stopPrice = input("Stop Price: ")
+    stopLoss = input("Stop Loss: ")
+    # takeProfit = input("Take Profit: ")
+    # side = input("Side (1-Buy 2-Sell): ")
+    # productType = input("Product Type (Intraday/CO/BO): ")
 
     data = {
         "symbol":symbol,
-        "qty":qty,
-        "type":type,
-        "side":side,
-        "productType":productType.upper(),
-        "limitPrice":limitPrice,
-        "stopPrice":stopPrice,
-        "takeProfit": takeProfit,
+        "qty":int(qty),
+        "type":1,
+        "side":-1,
+        "productType":"CO",   #productType.upper(),
+        # "price":float(price),
+        "limitPrice":float(limitPrice),
+        # "stopPrice":0,
+        # "takeProfit": 0,
         "validity":"DAY",
         "disclosedQty":0,
         "offlineOrder":False,
-        "orderTag":"tag1"
+        # "orderTag":"tag1",
+        "stopLoss":float(stopLoss)
     }
+    print(data)
     response = fyers.place_order(data=data)
     print(response)
 
@@ -85,6 +100,7 @@ def getOrderbook():
              )
         _orderNo = _orderNo + 1
 
-getOrderbook()
-# print('--------Lets create a New Order:------------')
+# getOrderbook()
+getTradeToOpen()
+print('--------Lets create a New Order:------------')
 # openNewOrder()
