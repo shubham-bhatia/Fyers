a66f85d84de11db2e390e96c8bea8ae1fc51f274
shubhbhatia <shubhambhatiak@gmail.com>
2024-08-22 10:56:48 +0530

    Commit

--- generateToken.py
+++ generateToken.py
@@ -1,28 +1,31 @@
 import json
 import requests
 import pyotp
+import hashlib
 from urllib import parse
 import sys
-import hashlib
-from fyers_apiv3 import fyersModel
 
-# Client Info (ENTER YOUR OWN INFO HERE!! Data varies from users and app types)
-FY_ID = "XS99684"  # Your fyers ID
-APP_ID_TYPE = "2"  # Keep default as 2, It denotes web login
-TOTP_KEY = "6DWVRNJZCAVA4MKKLFWEICT342L34D57"  # TOTP secret is generated when we enable 2Factor TOTP from myaccount portal
-PIN = "7777"  # User pin for fyers account#
-APP_ID = "JEBUMTMO2I"  # "7ZZHXHVNKN"  # App ID from myapi dashboard is in the form appId-appType. Example - EGNI8CE27Q-100, In this code EGNI8CE27Q will be APP_ID and 100 will be the APP_TYPE
-REDIRECT_URI = "https://www.google.com"  # Redirect url from the app.
+# This script will only work if TOTP is enabled.
+# You can enable TOTP using this link: https://myaccount.fyers.in/ManageAccount >> External 2FA TOTP >> click on "Enable".
+
+# Client Information (ENTER YOUR OWN INFO HERE! Data varies from users and app types)
+CLIENT_ID = "XS99684"  # Your Fyers Client ID
+PIN = "6666"  # User pin for Fyers account
+APP_ID = "JEBUMTMO2I"  # App ID from MyAPI dashboard (https://myapi.fyers.in/dashboard). The format is appId-appType.
+# Example: YIXXXXXSE-100. In this code, YIXXXXXSE is the APP_ID and 100 is the APP_TYPE
 APP_TYPE = "100"
-# APP_ID_HASH = "217c9b9e506820e731a58284bf09f994bf9b58243e5f100755483fdefb11dabc"  # SHA-256 hash of appId-appType:appSecret
-a_string = 'JEBUMTMO2I-100:MTHAV8WWP8'  # NEW
-APP_ID_HASH = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+APP_SECRET = "MTHAV8WWP8"  # App Secret from myapi dashboard (https://myapi.fyers.in/dashboard)
+TOTP_SECRET_KEY = "OG27GYY7NNEBRMJEAFYBYUB76VT7SUCU"  # TOTP secret key, copy the secret while enabling TOTP.
+
+REDIRECT_URI = "https://www.google.com"  # Redirect URL from the app
+
+# NOTE: Do not share these secrets with anyone.
+
 
 # API endpoints
 BASE_URL = "https://api-t2.fyers.in/vagator/v2"
-BASE_URL_2 =  "https://api.fyers.in/api/v2"
-# BASE_URL_2 =  "https://api-t1.fyers.in/api/v3"
-URL_SEND_LOGIN_OTP = BASE_URL + "/send_login_otp"
+BASE_URL_2 = "https://api-t1.fyers.in/api/v3"
+URL_VERIFY_CLIENT_ID = BASE_URL + "/send_login_otp"
 URL_VERIFY_TOTP = BASE_URL + "/verify_otp"
 URL_VERIFY_PIN = BASE_URL + "/verify_pin"
 URL_TOKEN = BASE_URL_2 + "/token"
@@ -31,14 +34,16 @@ URL_VALIDATE_AUTH_CODE = BASE_URL_2 + "/validate-authcode"
 SUCCESS = 1
 ERROR = -1
 
-def send_login_otp(fy_id, app_id):
+
+def verify_client_id(client_id):
     try:
         payload = {
-            "fy_id": fy_id,
-            "app_id": app_id
+            "fy_id": client_id,
+            "app_id": "2"
         }
 
-        result_string = requests.post(url=URL_SEND_LOGIN_OTP, json=payload)
+        result_string = requests.post(url=URL_VERIFY_CLIENT_ID, json=payload)
+        # print("result_string : ", result_string.text)
         if result_string.status_code != 200:
             return [ERROR, result_string.text]
 
@@ -50,10 +55,11 @@ def send_login_otp(fy_id, app_id):
     except Exception as e:
         return [ERROR, e]
 
+
 def generate_totp(secret):
     try:
         generated_totp = pyotp.TOTP(secret).now()
-        print("generated_totp: ", generated_totp)
+        print(generated_totp)
         return [SUCCESS, generated_totp]
 
     except Exception as e:
@@ -101,10 +107,10 @@ def verify_PIN(request_key, pin):
         return [ERROR, e]
 
 
-def token(fy_id, app_id, redirect_uri, app_type, access_token):
+def token(client_id, app_id, redirect_uri, app_type, access_token):
     try:
         payload = {
-            "fyers_id": fy_id,
+            "fyers_id": client_id,
             "app_id": app_id,
             "redirect_uri": redirect_uri,
             "appType": app_type,
@@ -134,8 +140,16 @@ def token(fy_id, app_id, redirect_uri, app_type, access_token):
         return [ERROR, e]
 
 
-def validate_authcode(app_id_hash, auth_code):
+def sha256_hash(appId, appType, appSecret):
+    message = f"{appId}-{appType}:{appSecret}"
+    message = message.encode()
+    sha256_hash = hashlib.sha256(message).hexdigest()
+    return sha256_hash
+
+
+def validate_authcode(auth_code):
     try:
+        app_id_hash = sha256_hash(appId=APP_ID, appType=APP_TYPE, appSecret=APP_SECRET)
         payload = {
             "grant_type": "authorization_code",
             "appIdHash": app_id_hash,
@@ -156,31 +170,31 @@ def validate_authcode(app_id_hash, auth_code):
 
 
 def main():
-    # Step 1 - Retrieve request_key from send_login_otp API
-    send_otp_result = send_login_otp(fy_id=FY_ID, app_id=APP_ID_TYPE)
-    if send_otp_result[0] != SUCCESS:
-        print(f"send_login_otp failure - {send_otp_result[1]}")
+    # Step 1 - Retrieve request_key from verify_client_id Function
+    verify_client_id_result = verify_client_id(client_id=CLIENT_ID)
+    if verify_client_id_result[0] != SUCCESS:
+        print(f"verify_client_id failure - {verify_client_id_result[1]}")
         sys.exit()
     else:
-        print("send_login_otp success")
+        print("verify_client_id success")
 
     # Step 2 - Generate totp
-    generate_totp_result = generate_totp(secret=TOTP_KEY)
+    generate_totp_result = generate_totp(secret=TOTP_SECRET_KEY)
     if generate_totp_result[0] != SUCCESS:
         print(f"generate_totp failure - {generate_totp_result[1]}")
         sys.exit()
     else:
         print("generate_totp success")
 
-    # Step 3 - Verify totp and get request key from verify_otp API
-    request_key = send_otp_result[1]
+    # Step 3 - Verify totp and get request key from verify_totp Function.
+    request_key = verify_client_id_result[1]
     totp = generate_totp_result[1]
     verify_totp_result = verify_totp(request_key=request_key, totp=totp)
     if verify_totp_result[0] != SUCCESS:
         print(f"verify_totp_result failure - {verify_totp_result[1]}")
         sys.exit()
     else:
-        print("verify_totp_result success:", totp)
+        print("verify_totp_result success")
 
     # Step 4 - Verify pin and send back access token
     request_key_2 = verify_totp_result[1]
@@ -191,9 +205,9 @@ def main():
     else:
         print("verify_pin_result success")
 
-    # Step 5 - Get auth code for API V2 App from trade access token
+    # Step 5 - Get auth code for API V3 App from trade access token
     token_result = token(
-        fy_id=FY_ID, app_id=APP_ID, redirect_uri=REDIRECT_URI, app_type=APP_TYPE,
+        client_id=CLIENT_ID, app_id=APP_ID, redirect_uri=REDIRECT_URI, app_type=APP_TYPE,
         access_token=verify_pin_result[1]
     )
     if token_result[0] != SUCCESS:
@@ -202,21 +216,19 @@ def main():
     else:
         print("token_result success")
 
-    # Step 6 - Get API V2 access token from validating auth code
+    # Step 6 - Get API V3 access token from validating auth code
     auth_code = token_result[1]
-    validate_authcode_result = validate_authcode(
-        app_id_hash=APP_ID_HASH, auth_code=auth_code
-    )
+    validate_authcode_result = validate_authcode(auth_code=auth_code)
     if token_result[0] != SUCCESS:
         print(f"validate_authcode failure - {validate_authcode_result[1]}")
         sys.exit()
     else:
         print("validate_authcode success")
 
-    # access_token = APP_ID + "-" + APP_TYPE + ":" + validate_authcode_result[1]
-    access_token = validate_authcode_result[1]
-    return access_token
+    access_token = APP_ID + "-" + APP_TYPE + ":" + validate_authcode_result[1]
+
+    print(f"\naccess_token - {access_token}\n")
+
 
 if __name__ == "__main__":
-    generatedAccessToken = main()
-    # print('generatedAccessToken', generatedAccessToken)
+    main()
